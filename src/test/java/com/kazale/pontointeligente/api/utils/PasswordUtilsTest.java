3daac6e224a1f0f9d32f6fe0ec083c59ec5d0e1b
marta silva <martaraquel90@gmail.com>
2020-08-27 17:32:57 +0100

    Adicionar Entidades, Repositorio e Testes

---
+++ src/test/java/com/kazale/pontointeligente/api/utils/PasswordUtilsTest.java
@@ -0,0 +1,25 @@
+package com.kazale.pontointeligente.api.utils;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtilsTest {
+	
+	private static final String SENHA = "123456";
+	private final BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+
+	@Test
+	public void testSenhaNula() throws Exception {
+		 Assertions.assertNull(PasswordUtils.gerarBCrypt(null));
+	}
+	
+	@Test
+	public void testGerarHashSenha() throws Exception {
+		String hash = PasswordUtils.gerarBCrypt(SENHA);
+		
+		 Assertions.assertTrue(bCryptEncoder.matches(SENHA, hash));
+	}
+
+}
